25e4e8f303e72b224d92d7b595c425e87ab71e36
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-15 17:56:20 +0900

    D-2
    v-1.0#2
    ---
    1. figuring out => receive JSON object with javascript
    
    
    	==> DROP

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -134,8 +134,124 @@ _modify_content__Done
 	
 }//_modify_content__Done
 
-$(document).ready(function(){
+function get_kw_mix() {
+
+	/***************************
+		change color
+	 ***************************/
+	$label = $('#message_area');
 	
+	$label.css("background", "yellow");
+
+	/***************************
+		set: url
+	 ***************************/
+	var hostname = window.location.hostname;
+	
+	var url;
+	
+	if (hostname == "benfranklin.chips.jp") {
+		
+		url = "/cake_apps/Cake_IG/keywords/get_keywords_mix/3";
+//		url = "/cake_apps/Cake_NR5/keywords/add_KW__Genre_Changed";
+		
+	} else {
+	
+		url = "/Eclipse_Luna/Cake_IG/keywords/get_keywords_mix/3";
+//		url = "/Cake_IG/keywords/get_keywords_mix/3";
+//		url = "/Cake_NR5/keywords/add_KW__Genre_Changed";
+	
+	}
+
+//	alert(url);
+	
+	/***************************
+		ajax
+	 ***************************/
+	$.ajax({
+		
+	    url: url,
+	    type: "GET",
+	    //REF http://stackoverflow.com/questions/1916309/pass-multiple-parameters-to-jquery-ajax-call answered Dec 16 '09 at 17:37
+//	    data: {id: id},
+	    
+	    timeout: 10000
+	    
+	}).done(function(data, status, xhr) {
+
+//		alert(data);
+		
+		_get_kw_mix__Done(data);
+		
+	}).fail(function(xhr, status, error) {
+		
+		alert(xhr.status);
+		
+	});
+
+	
+}
+
+function _get_kw_mix__Done(data) {
+
+	/***************************
+			change color
+		 ***************************/
+		$label = $('#message_area');
+		
+		$label.css("background", "white");
+
+	alert(data);
+
+//	alert(data.length);
+	
+//	var text = '{ "employees" : {' +					//=> n/w
+//	'{ "firstName":"John" , "lastName":"Doe" },' +
+//	'{ "firstName":"Anna" , "lastName":"Smith" },' +
+//	'{ "firstName":"Peter" , "lastName":"Jones" } }}';
+	var text = '{ "employees" : [' +					//=> w
+	'{ "firstName":"John" , "lastName":"Doe" },' +
+	'{ "firstName":"Anna" , "lastName":"Smith" },' +
+	'{ "firstName":"Peter" , "lastName":"Jones" } ],'+
+	'"employees" : [' +
+	           	'{ "firstName":"John" , "lastName":"Doe" },' +
+	           	'{ "firstName":"Anna" , "lastName":"Smith" },' +
+	           	'{ "firstName":"Peter" , "lastName":"Jones" } ]' +
+	'}';
+//	var text = '{ "employees" : [' +
+//	'{ "firstName":"John" , "lastName":"Doe" },' +
+//	'{ "firstName":"Anna" , "lastName":"Smith" },' +
+//	'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+	
+	var obj = JSON.parse(text);
+	
+	alert("parsed");
+	
+	/***************************
+		decode
+	 ***************************/
+//	var j = JSON.parse(data);
+//	
+//	alert("parsed");
+//	alert(j);
+	
+//	var j = data;
+//	
+////	alert(j.length);
+//	
+////	j_cropped = j.substring(20, j.length - 10);
+//	j_cropped = j.substring(20, j.length - 1);
+////	j_cropped = j.substring(2,j.length - 1);
+//	
+//	alert(j_cropped);
+//    obj = JSON.parse(j);
+//
+//	alert(obj.count);
+	
+}
+
+$(document).ready(function(){
+
 //	alert("ready");
 
     
